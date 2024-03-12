827685989c2d10a18e88bf1f3dbcc707b208d568
Violinie <hancacopakova@gmail.com>
2024-03-12 18:52:59 +0100

    write first a from input followed by new line

---
+++ aff_a/aff_a.c
@@ -0,0 +1,35 @@
+/*Write a program that takes a string, and displays the first 'a' character it
+encounters in it, followed by a newline. If there are no 'a' characters in the
+string, the program just writes a newline. If the number of parameters is not
+1, the program displays 'a' followed by a newline.
+
+Example:
+$> ./aff_a "abc" | cat -e
+a$
+$> ./aff_a "dubO a POIL" | cat -e
+a$
+$> ./aff_a "zz sent le poney" | cat -e
+$
+$> ./aff_a | cat -e
+a$*/
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    int i;
+    if (argc > 1)
+        i = 0;
+        while (argv[1])
+        {
+            if (argv[1][i] == 'a')
+            {
+                write(1, "a\n", 2);
+                return 0;
+            }
+            else 
+                write(1, "\n", 1);
+            i++;
+        }
+    return (0);
+}
